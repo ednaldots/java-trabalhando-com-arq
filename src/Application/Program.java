0f88a997a8ad949359aac0d9db7a44983bcd5bce
ednaldo <ednaldo.contato1@gmail.com>
2024-02-25 18:27:55 -0300

    4-manipulando-pastas-com-file

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,26 +1,35 @@
 package Application;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		String[] lines = new String[] { "Goode morning", "Good afternoon", "Good night"};
+		Scanner sc = new Scanner(System.in);
 		
-		String path = "c:\\temp\\out.txt";
+		System.out.println("Enter a folder path: ");
+		String strPath = sc.nextLine();
 		
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
-			for (String line : lines) {
-				bw.write(line);
-				bw.newLine();
-			}
-
+		File path = new File(strPath);
+		
+		File[] folders = path.listFiles(File::isDirectory);
+		System.out.println("FOLDERS:");
+		for (File folder : folders) {
+			System.out.println(folder);
 		}
-		catch (IOException e) {
-			e.printStackTrace();
+		
+		File[] files = path.listFiles(File::isFile);
+		System.out.println("FILES:");
+		for (File file : files) {
+			System.out.println(file);
 		}
+		
+		boolean success = new File(strPath + "\\subdir").mkdir();
+		System.out.println("Directory created successfully: " +success);
+		
+		sc.close();
+		
 	}
 } 
