4dbf139c672a701d97608640209a04f4adbfb376
ednaldo <ednaldo.contato1@gmail.com>
2024-02-25 12:34:41 -0300

    file-writer-e-bufferd-writer

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,38 +1,26 @@
 package Application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		String path = ("c:\\temp\\in.txt");
-		FileReader fr = null;
-		BufferedReader br = null;
-
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-
-			String line = br.readLine();
-
-			while (line != null) {
-				System.out.println(line);
-				line = br.readLine();
+		String[] lines = new String[] { "Goode morning", "Good afternoon", "Good night"};
+		
+		String path = "c:\\temp\\out.txt";
+		
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
 			}
 
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
-		} finally {
-			try {
-				if (br != null) {
-					fr.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
-}
+} 
